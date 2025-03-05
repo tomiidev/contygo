e9958107fea86db100afdd6db9b37e6a1bf52a3a
tomiidev <tomasruglio21@gmail.com>
2025-03-05 13:15:00 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "dist"
+  },
   resolve: {
     alias: {
       '@': '/src',  // Aquí indicamos que "@" debe apuntar a la carpeta "src"
