a5ea57cde649db08b9e95b20358ca5e3c60ebf9d
tomiidev <tomasruglio21@gmail.com>
2025-03-05 15:42:49 -0300

    signup

--- src/pages/Authentication/SignUp.tsx
+++ src/pages/Authentication/SignUp.tsx
@@ -108,23 +108,23 @@ const SignUp: React.FC = () => {
 
         <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
           <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
-            <span className="mb-1.5 block font-medium">Start for free</span>
+            <span className="mb-1.5 block font-medium">¡7 dias gratis!</span>
             <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
-              Sign Up to TailAdmin
+              Registrate en Contygo
             </h2>
 
             <form onSubmit={handleSubmit}>
               {/* Name */}
               <div className="mb-4">
                 <label className="mb-2.5 block font-medium text-black dark:text-white">
-                  Name
+                  Nombre
                 </label>
                 <div className="relative">
                   <input
                     type="text"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
-                    placeholder="Enter your full name"
+                    placeholder="Nombre"
                     className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                   />
                   {errors.name && <span className="text-red-500">{errors.name}</span>}
@@ -141,7 +141,7 @@ const SignUp: React.FC = () => {
                     type="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter your email"
+                    placeholder="Email"
                     className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                   />
                   {errors.email && <span className="text-red-500">{errors.email}</span>}
@@ -151,14 +151,14 @@ const SignUp: React.FC = () => {
               {/* Password */}
               <div className="mb-4">
                 <label className="mb-2.5 block font-medium text-black dark:text-white">
-                  Password
+                  Contraseña
                 </label>
                 <div className="relative">
                   <input
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter your password"
+                    placeholder="Contraseña"
                     className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                   />
                   {errors.password && <span className="text-red-500">{errors.password}</span>}
@@ -168,14 +168,14 @@ const SignUp: React.FC = () => {
               {/* Confirm Password */}
               <div className="mb-6">
                 <label className="mb-2.5 block font-medium text-black dark:text-white">
-                  Re-type Password
+                  Confirma la contraseña
                 </label>
                 <div className="relative">
                   <input
                     type="password"
                     value={confirmPassword}
                     onChange={(e) => setConfirmPassword(e.target.value)}
-                    placeholder="Re-enter your password"
+                    placeholder="Confirma la contraseña"
                     className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                   />
                   {errors.confirmPassword && (
@@ -188,7 +188,7 @@ const SignUp: React.FC = () => {
               <div className="mb-5">
                 <input
                   type="submit"
-                  value={loading ? "Creating..." : "Create account"}
+                  value={loading ? "Creando cuenta..." : "Registrarse"}
                   className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 text-white transition hover:bg-opacity-90"
                   disabled={loading}
                 />
